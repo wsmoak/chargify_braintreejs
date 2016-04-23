06794f7591c3bbf90eb5eaf8781ebc0b59bf3a55
Wendy Smoak <wsmoak@gmail.com>
2016-04-23 18:15:45 -0400

    Create a Braintree Customer and a Chargify Subscription

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,2 +1,66 @@
 class SignupController < ApplicationController
+
+  def index
+    Braintree::Configuration.environment = :sandbox
+    Braintree::Configuration.merchant_id = ENV["BRAINTREE_MERCHANT_ID"]
+    Braintree::Configuration.public_key = ENV["BRAINTREE_PUBLIC_KEY"]
+    Braintree::Configuration.private_key = ENV["BRAINTREE_PRIVATE_KEY"]
+
+    @token = Braintree::ClientToken.generate
+  end
+
+  def create
+    first_name = params[:first_name]
+    last_name = params[:last_name]
+    email = params[:email]
+
+    nonce = params[:payment_method_nonce]
+
+    result = Braintree::Customer.create(
+      :first_name => first_name,
+      :last_name => last_name,
+      :email => email,
+      :payment_method_nonce => nonce
+    )
+
+    puts "******* BRAINTREE RESULT ********"
+    puts result.inspect
+
+    if result.success?
+      vault_token = result.customer.id
+    else
+      raise result.errors
+    end
+
+    Chargify.configure do |c|
+      c.api_key   = ENV["CHARGIFY_API_KEY"]
+      c.subdomain = ENV["CHARGIFY_BRAINTREE_SUBDOMAIN"]
+    end
+
+    subscription = Chargify::Subscription.create(
+      :product_handle => 'basic',
+      :customer_attributes => {
+        :first_name => first_name,
+        :last_name => last_name,
+        :email => email
+      },
+      :credit_card_attributes => {
+        :first_name => first_name,
+        :last_name => last_name,
+        :vault_token => result.customer.id,
+        :card_type => result.customer.credit_cards[0].card_type.downcase,
+        :expiration_month => result.customer.credit_cards[0].expiration_month,
+        :expiration_year => result.customer.credit_cards[0].expiration_year,
+        :current_vault => "braintree_blue"
+      }
+    )
+
+    puts "****** CHARGIFY SUBSCRIPTION ******"
+    puts subscription.inspect
+
+    if subscription.errors.any?
+      raise subscription.errors.full_messages.join(", ")
+    end
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :signup, only: [:index, :create]
+
   # Example resource route with options:
   #   resources :products do
   #     member do
